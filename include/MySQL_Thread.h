357fd755973623595d64e550e53d9952c72056fb
Akshay Suryawanshi <akshay.suryawanshi@shopify.com>
2020-01-24 04:41:36 +0000

    Global connection warming support

--- include/MySQL_Thread.h
+++ include/MySQL_Thread.h
@@ -292,6 +292,7 @@ class MySQL_Threads_Handler
 		int connect_timeout_server_max;
 		int free_connections_pct;
 		int show_processlist_extended;
+		bool connection_warming;
 #ifdef IDLE_THREADS
 		int session_idle_ms;
 		bool session_idle_show_processlist;
